b90abf356075335050c4ea8c9bd5fefe720fdbaa
Adhitya Mohan <me@adhityamohan.in>
2020-09-11 23:41:21 +0530

    sdm845-common: Build signapk - Zipping an ota needs this  TDL: commonize and move to vendor
    Signed-off-by: ALKALiKong <alikongalk@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -310,6 +310,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
