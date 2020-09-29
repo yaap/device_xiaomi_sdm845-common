48907ff9fef2989636d30ebf89859d25487c7f28
PIPIPIG233666 <pig.priv@gmail.com>
2020-09-29 19:50:03 +0800

    sdm845-common: fingerprint: Uprev to v2.2
    Signed-off-by: ALKALiKong <alikongalk@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -184,7 +184,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845
+    android.hardware.biometrics.fingerprint@2.2-service.xiaomi_sdm845
 
 # Health
 PRODUCT_PACKAGES += \
