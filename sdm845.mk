5954206bbf0acea73f298d02135c83666cb6b30d
ALKALiKong <alikongalk@gmail.com>
2020-11-14 10:28:38 +0800

    sdm845-common: usb: Rename basic_sdm845 to basic
    Signed-off-by: ALKALiKong <alikongalk@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -355,7 +355,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic_sdm845
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
