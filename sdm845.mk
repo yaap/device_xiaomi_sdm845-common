c98f02997e6ee107c0daa6d9db58ee1fe1270c27
Bruno Martins <bgcngm@gmail.com>
2020-11-23 22:12:56 +0000

    sdm845-common: Ditch VR HAL
    Change-Id: Id85d2dd3eb1694cbc6c5246f2aeb92462d15f2cf
    Signed-off-by: ALKALiKong <alikongalk@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -356,12 +356,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl:64 \
     android.hardware.vibrator@1.0-service
 
-# VR
-PRODUCT_PACKAGES += \
-    android.hardware.vr@1.0-impl \
-    android.hardware.vr@1.0-service \
-    vr.sdm845
-
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
