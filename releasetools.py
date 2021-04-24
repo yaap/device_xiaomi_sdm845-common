721cfbbd772832b5bec50a720643cb38c4d9116a
Bruno Martins <bgcngm@gmail.com>
2021-04-24 15:48:15 +0100

    sdm845-common: Build disabled VBMeta image for all targets
    This was never needed for any mi845 target besides perseus
    (all the rest seem to have just empty partitions),
    but should be set here so it fixes signing builds with own keys.
    
    Change-Id: Idded651f3b23535581a555bc2a27409489d8da1b
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: ALKALiKong <alikongalk@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -44,6 +44,7 @@ def OTA_UpdateFirmware(info):
 
 def OTA_InstallEnd(info, input_zip):
   AddImage(info, input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   return
 
 def AddBasebandAssertion(info, input_zip):
